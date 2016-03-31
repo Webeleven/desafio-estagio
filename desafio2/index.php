e6011ff2dbe1d39aa9801c9ad2bda7bcb5619019
Joel Santos <joel.santos.br@gmail.com>
2016-03-31 14:16:56 -0300

    Desafios completos

--- desafio2/index.php
+++ desafio2/index.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Calculadora</title>
+    <title>Desafio Calculadora</title>
     <link rel="stylesheet" href="css/style.css" type="text/css">
   </head>
   <body>
